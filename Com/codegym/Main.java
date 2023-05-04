45cbe7f440aa6379f354b0e0a6fff070a5e8a21e
hoangquan2106 <hthquan21699@gmail.com>
2023-05-04 16:02:54 +0700

    bai3

---
+++ Com/codegym/Main.java
@@ -0,0 +1,20 @@
+package Com.codegym;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        float width;
+        float height;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter width: ");
+        width = scanner.nextFloat();
+        System.out.println("Enter height: ");
+        height = scanner.nextFloat();
+        float are = width * height;
+        System.out.println("area is" + are);
+
+    }
+}
